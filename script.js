e466e478d774b1b829f90238ad5d6bb4574e5a7b
yoancolina-rgb <yoan.colina@escola.pr.gov.br>
2025-10-23 17:17:07 -0300

    ...

---
+++ script.js
@@ -0,0 +1,8 @@
+const caixaPergunta = document.querySelector("caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+    enunciado:""
